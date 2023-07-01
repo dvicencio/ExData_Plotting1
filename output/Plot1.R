d13f6575f4f4c98b3fad60472d5122174203e9b7
Daniel <dvicencio947@gmail.com>
2023-07-01 19:59:49 +0100

    Ready

--- output/Plot1.R
+++ output/Plot1.R
@@ -8,5 +8,11 @@ data <- subset(data_power, Date >= "2007-02-01" & Date <= "2007-02-02")
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
+png("output/plot1.png", width = 480, height = 480)
+
 hist(data$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+
+
+dev.off()
